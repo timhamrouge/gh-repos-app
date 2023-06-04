2282d70c3e276a55be9b7a5f740da41ca7d7dac2
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-06-04 19:04:46 +0100

    more setup

---
+++ src/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #FFFFFF;
+    font-family: 'Atkinson+Hyperlegible', sans-serif;
+  }
+`
+
+export default GlobalStyle;
